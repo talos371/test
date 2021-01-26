73db068111db04ebbb1db12452fd0281ecb401b9
Zohrabyan, Davit <davit.zohrabyan@salt-solutions.de>
2021-01-26 08:35:06 +0100

    test11

--- .gitlab/MAIN-github-copy.sh
+++ .gitlab/MAIN-github-copy.sh
@@ -1,54 +1,36 @@
 #!/bin/bash
 
-
 source .gitlab/common.sh
 
-# shellcheck disable=SC2154
+project_name="$(echo "$project" | awk -F "/" '{print $NF}' | awk -F "." '{print $1}')"
+printf "Project name: %s\n" "$project_name"
+printf "GIT repo: %s\n" "$project"
 
-  project="https://swugit1.salt-solutions.de/sce/common/github_test.git"
-  project_name="$(echo "$project" | awk -F "/" '{print $NF}' | awk -F "." '{print $1}')"
-  printf "Project name: %s\n" "$project_name"
-  printf "GIT repo: %s\n" "$project"
+printf "Clone git repository...\n"
 
-  printf "Clone git repository...\n"
-  # shellcheck disable=SC2001
+gitlab_address="https://$gitlab_user:$gitlab_token@$(echo "$project" | sed -e 's#https://##')"
+git clone "$gitlab_address"
 
-  gitlab_address="https://$gitlab_user:$gitlab_token@$(echo "$project" | sed -e 's#https://##')"
-  git clone "$gitlab_address"
+cd "$project_name" || exit
 
-  cd "$project_name"
+printf "Delete pipeline tags - if exists...\n"
+for tag in $(git tag | grep pipeline); do
+  git tag -d "$tag"
+  git push --delete origin "$tag"
+done
 
-  printf "Delete pipeline tags - if exists...\n"
-  for tag in $(git tag | grep pipeline); do
-    git tag -d "$tag"
-    git push --delete origin "$tag"
-  done
+printf "Delete gitlab-ci file and gitlab folder...\n"
+git rm .gitlab-ci.yml
+git rm -rf .gitlab
 
-  printf "1...\n"
-  pwd
-  ls -a
+github_address="https://$github_user:$github_password@$(echo "$github_url" | sed -e 's#https://##')"
+printf "Copy into $github_address\n"
 
-  printf "Delete gitlab-ci file and gitlab folder...\n"
-  # rm -f .gitlab-ci.yml
-  # rm -Rf .gitlab
-
-  git rm .gitlab-ci.yml
-  git rm -rf .gitlab
-
-  printf "2...\n"
-  pwd
-  ls -a
-
-  github_address="https://$github_user:$github_password@$(echo "$github_url" | sed -e 's#https://##')"
-  printf "Copy into $github_address\n"
-
-  git config --global user.email "you@example.com"
-  git config --global user.name "Your Name"
-  git remote add github "$github_address"
-  git commit -m "added my github name"
-  git push github master -f
-
-  # git push --mirror -f "$github_address"
+git config --global user.email "$github_user_email"
+git config --global user.name "$github_user"
+git remote add github "$github_address"
+git commit -m "$github_commit_message"
+git push github master -f
 
 printf "\n"
 printf "==============================================================================================================\n"
